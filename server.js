a7a6b3f574737b40b8ab20f0c90c7176261f0bec
Simon Crabtree <limajs@simoncrabtree.net>
2013-04-02 16:45:32 +0100

    Extract model out into variable

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ server.configure(function () {
     server.use(express.static(__dirname + '/client'));
 });
 
-server.get('/query/shoppingList', function (req, res) {
-    res.json({
+var shoppingListViewModel = {
         summaryMessage: "You need to purchase 3 Items",
         shoppingListItems: [
             {description: "Baked beans"},
             {description: "Toilet roll"},
             {description: "Eggs"}
         ]
-    });
+    };
+
+
+server.get('/query/shoppingList', function (req, res) {
+    res.json(shoppingListViewModel);
 });
 
 server.listen(3000);
