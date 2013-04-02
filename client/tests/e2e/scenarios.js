06a203ac747e8f61e80a0a5b786c2b60ca3ac7e2
Simon Crabtree <limajs@simoncrabtree.net>
2013-04-02 16:24:04 +0100

    Default routing test added

--- client/tests/e2e/scenarios.js
+++ client/tests/e2e/scenarios.js
@@ -1,15 +1,27 @@
 describe("Application", function () {
+  it("Default page is the Home View", function () {
+      browser().navigateTo('../../index.html');
+      expect(browser().location().url()).toBe('/home');
+  });
+
   describe("Home View", function () {
     beforeEach(function () {
-      browser().navigateTo("../../index.html");
+      browser().navigateTo("../../index.html#/home");
     });
 
     it("Should display Home Page", function () {
-      expect(element('#message').text()).toBe("Welcome to the app");
+      expect(element('#message').text()).toBe("You need to purchase 3 Items");
     });
 
     it("Displays the shopping list items", function () {
       expect(repeater('.shopping-list li').count()).toBe(3);
+    });
+  });
+
+  describe("About View", function () {
+    it("Is reached from #/about", function () {
+      browser().navigateTo('../../index.html#/about');
+      expect(browser().location().url()).toBe('/about');
     });
   });
 });
